75f7cba0efdd60fc2a61edd5162b063fe7737e7b
ligang <913489864@qq.com>
2020-10-11 17:26:25 +0800

    修改操作

--- src/main/java/com/ligangit/controller/HelloController.java
+++ src/main/java/com/ligangit/controller/HelloController.java
@@ -17,7 +17,7 @@ public class HelloController {
     public void hello() {
         LOGGER.info("hello world！");
         LOGGER.info("检查代码！");
-        String AABBcc = "abnkfh";
-        System.out.println("helloceshicheihi");
+        String AABBcc = "abnkfdh";
+        System.out.println("helloceshichecihi");
     }
 }
