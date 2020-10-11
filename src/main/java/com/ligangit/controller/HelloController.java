491e30aba0b6d2b6b148a3e85aa3805683f46439
ligang <913489864@qq.com>
2020-10-11 14:52:48 +0800

    代码提交

---
+++ src/main/java/com/ligangit/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ligangit.controller;/*
+ * @BelongsProject:check_code
+ * @BelongsPackage:com.ligangit.controller
+ * @Author:com.ligangit.controller
+ * @CreateTime:2020-10-11 ${HORU}:42
+ * @Descriptation:todo*/
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloController {
+    private  final  static Logger logger = LoggerFactory.getLogger(HelloController. class);
+    public static void main(String[] args) {
+        logger.info("hello world！");
+        logger.info("检查代码！");
+    }
+}
