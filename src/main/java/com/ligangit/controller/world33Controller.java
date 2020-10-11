9822d6e531a4f4abf6b78961dd3cd2c4941cd690
ligang <913489864@qq.com>
2020-10-11 20:19:35 +0800

    ssss

---
+++ src/main/java/com/ligangit/controller/world33Controller.java
@@ -0,0 +1,23 @@
+package com.ligangit.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *  ceshi
+ *  @author ligang
+ *  @date 2020/10/11
+ */
+public class worldController {
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(worldController.class);
+
+    public void hello() {
+        LOGGER.info("hello world！");
+        LOGGER.info("检查代码！");
+        String AABBcc = "abnkvv33fdh";
+        System.out.println("helles33hichecihi");
+    }
+}
